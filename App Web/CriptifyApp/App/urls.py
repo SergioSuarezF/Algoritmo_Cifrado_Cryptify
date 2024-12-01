b704371a7cbcc8ae8d3222c714e6028d737fd673
Sergio Suarez <sergio.suarez@ucsm.edu.pe>
2024-12-01 13:51:00 -0500

    Iniciar Sesión

--- App Web/CriptifyApp/App/urls.py
+++ App Web/CriptifyApp/App/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('encrypt/', views.encrypt, name='encrypt'),
     path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
     
 ]
