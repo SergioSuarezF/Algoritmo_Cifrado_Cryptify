85d79a2312cc5918a89266eba4224be4cba9e77b
Sergio Suarez <sergio.suarez@ucsm.edu.pe>
2024-12-01 15:38:03 -0500

    Creacion de tabla Encript - Parcial

--- App Web/CriptifyApp/Claves/models.py
+++ App Web/CriptifyApp/Claves/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Encrypt(models.Model):
+    dif_date = models.IntegerField
+    mensaje_inicial = models.CharField(max_length=32)
+    clave = models.CharField(max_length=32)
+    len_clave_aes = models.IntegerField
+    clave_inv_sep = models.CharField(max_length=64)
+    mensaje_cifrado_aes = models.CharField(max_length=100)
+    mensaje_cifrado_final = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
+
+    
+    
+    
